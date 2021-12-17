b2f694d14767349c0b54ea2d42661dc8cb974714
rizka syaukany <rizka.syaukany@gmail.com>
2021-12-17 20:53:23 +0700

    final FE syaukany

--- index.php
+++ index.php
@@ -57,17 +57,19 @@
                     </select>
                     <label class="mt-3" for="namaNik">NAMA NIK PENERIMA MANFAAT</label>
                     <input class="form-control " id="namaNik" type="text" placeholder="Nama/NIK penerima manfaat">
-                    <a class="btn btn-warning btn-custom mt-3 float-center" href="#">Submit </a>
+                    <a class="btn btn-warning btn-custom mt-3 float-center" data-toggle="modal"
+                        data-target="#modalHasil">Submit </a>
                 </form>
             </div>
         </div>
     </div>
 
+    <!-- modal Login -->
     <div class="modal fade" id="loginModal" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Login</h5>
+            <div class="modal-content bg-lesu">
+                <div class="modal-header txt-center">
+                    <h5 class="modal-title ">Login</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -85,6 +87,54 @@
                         </div>
                         <button type="submit" class="btn btn-warning btn-custom">Submit</button>
                     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- modal Hasil -->
+    <div class="modal" id="modalHasil" tabindex="-1">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Hasil Pencarian</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
