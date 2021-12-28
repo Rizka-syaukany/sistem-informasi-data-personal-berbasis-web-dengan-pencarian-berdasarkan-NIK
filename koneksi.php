1b81bc7db32a76e93d395afcff5faee447bd3279
rizka syaukany <rizka.syaukany@gmail.com>
2021-12-28 21:52:25 +0700

    penutupan 28 des 2021

--- koneksi.php
+++ koneksi.php
@@ -5,7 +5,7 @@ $db_pass1 = '';
 $database1 = 'bansos';
 
 //koneksi ke database
-$bansos=new mysqli($db_host1, $db_user1, $db_pass1, $database1);
+$bansos = new mysqli($db_host1, $db_user1, $db_pass1, $database1);
 
 // if ($bansos->connect_error) { 
 // 	die("<b>Yahh!  gagal</b> : " . mysqli_connect_error()); 
